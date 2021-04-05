d20db8635315ec9c861272449fb8f5b8e245e4c9
cryptotester <49552918+cryptotester@users.noreply.github.com>
2021-04-05 17:29:19 +0200

    Update get-nfts-advanced

--- nft/get-nfts-advanced/index.js
+++ nft/get-nfts-advanced/index.js
@@ -4,10 +4,12 @@ const { NFT_API } = require('../../constants')
 const getNftsAdvanced = async ({
     owner,
     collection_name,
+    sort = 'created',
+    order = 'desc',
     page = 1,
     limit = 100
 }) => {
-    const url = `${NFT_API}/atomicassets/v1/assets?owner=${owner}&collection_name=${collection_name}&state=1&page=${page}&limit=${limit}&order=desc&sort=created`
+    const url = `${NFT_API}/atomicassets/v1/assets?owner=${owner}&collection_name=${collection_name}&state=1&sort=${sort}&order=${order}&page=${page}&limit=${limit}`
     const res = await fetch(url)
     const { data } = await res.json()
     return data
