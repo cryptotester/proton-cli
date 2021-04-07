5f4bfb6ed13f3ff077d61feddf1f1c2a1e6fa9c3
cryptotester <49552918+cryptotester@users.noreply.github.com>
2021-04-07 09:13:27 +0200

    Add template_id in the nfts-advanced query

--- nft/get-nfts-advanced/index.js
+++ nft/get-nfts-advanced/index.js
@@ -4,12 +4,13 @@ const { NFT_API } = require('../../constants')
 const getNftsAdvanced = async ({
     owner,
     collection_name = '',
+    template_id = '',
     sort = 'minted',
     order = 'asc',
     page = 1,
     limit = 100
 }) => {
-    const url = `${NFT_API}/atomicassets/v1/assets?owner=${owner}&collection_name=${collection_name}&sort=${sort}&order=${order}&page=${page}&limit=${limit}`
+    const url = `${NFT_API}/atomicassets/v1/assets?owner=${owner}&collection_name=${collection_name}&template_id=${template_id}&sort=${sort}&order=${order}&page=${page}&limit=${limit}`
     const res = await fetch(url)
     const { data } = await res.json()
     return data
