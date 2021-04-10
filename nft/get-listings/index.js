376d9c80e25601b63812a8900f9625dedc165a86
cryptotester <49552918+cryptotester@users.noreply.github.com>
2021-04-10 19:35:49 +0200

    Allow filtering listings by ids

--- nft/get-listings/index.js
+++ nft/get-listings/index.js
@@ -5,13 +5,14 @@ const getListings = async ({
     seller = '',
     collection_name = '',
     template_id = '',
+    sale_ids = '',
     symbol = '',
     sort = 'created',
     order = 'desc',
     page = 1,
     limit = 100
 }) => {
-    const res = await fetch(`${NFT_API}/atomicmarket/v1/sales?seller=${seller}&collection_name=${collection_name}&template_id=${template_id}&state=1&sort=${sort}&order=${order}&page=${page}&limit=${limit}`)
+    const res = await fetch(`${NFT_API}/atomicmarket/v1/sales?seller=${seller}&collection_name=${collection_name}&template_id=${template_id}&ids=${sale_ids}&state=1&sort=${sort}&order=${order}&page=${page}&limit=${limit}`)
     const { data } = await res.json()
     return data
 }
