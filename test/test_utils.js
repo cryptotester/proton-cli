7239b3e5c83362d33874a1b408b959e8660eba1d
cryptotester <49552918+cryptotester@users.noreply.github.com>
2021-04-11 17:04:25 +0200

    Add getQuantity & getHumanFriendlyAmount & tests

---
+++ test/test_utils.js
@@ -0,0 +1,35 @@
+const expect = require('chai').expect
+const { getQuantity, getHumanFriendlyAmount } = require('../utils')
+
+describe('getQuantity()', function() {
+    const tests = [
+        {name : 'FOOBAR integer', amount: 1, symbol: 'FOOBAR', expected: '1.000000 FOOBAR'},
+        {name : 'XPR integer lowercase', amount: 100, symbol: 'xpr', expected: '100.0000 XPR'},
+        {name : 'XPR with decimals', amount: 1.0001, symbol: 'XPR', expected: '1.0001 XPR'},
+        {name : 'XPR with too many decimals (rounded minus)', amount: 1.00004, symbol: 'XPR', expected: '1.0000 XPR'},
+        {name : 'XPR with too many decimals (rounded plus)', amount: 1.00005, symbol: 'XPR', expected: '1.0001 XPR'},
+        ];
+
+    tests.forEach((test) => {
+        it(test.name, function() {
+            const result = getQuantity(test.amount, test.symbol)
+            expect(result).to.equal(test.expected)
+        })
+    })
+})
+
+  describe('getHumanFriendlyAmount()', function() {
+    const tests = [
+        {name : 'FOOBAR integer', amount: 1000000, symbol: 'FOOBAR', expected: 1},
+        {name : 'FOOBAR float', amount: 1100000, symbol: 'FOOBAR', expected: 1.1},
+        {name : 'FOOBAR float', amount: 1000001, symbol: 'FOOBAR', expected: 1.000001},
+        {name : 'FOOBAR float', amount: 0000001, symbol: 'FOOBAR', expected: 0.000001},
+      ];
+
+    tests.forEach((test) => {
+        it(test.name, function() {
+            const result = getHumanFriendlyAmount(test.amount, test.symbol)
+            expect(result).to.equal(test.expected)
+        })
+    })
+})
--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const { readdirSync } = require('fs')
 const { join } = require('path')
+const { TOKEN_PRECISIONS } = require('./constants')
 
 const getDeployableFilesFromDir = (dir) => {
     const dirCont = readdirSync(dir)
@@ -27,8 +28,34 @@ const sortDictionary = (unordered) => Object.keys(unordered).sort().reduce(
   {}
 );
 
+/**
+ * Get quantity for an API buy or sell request, e.g. 100.1 FOOBAR returns `100.100000 FOOBAR`
+ * @param amount The amount, e.g. 100.1
+ * @param symbol The symbol, e.g. FOOBAR
+ * @returns {String} Amount for API request, e.g. 100.100000 FOOBAR
+ */
+const getQuantity = (amount, symbol) => {
+  symbol = symbol.toUpperCase()
+  const precision = TOKEN_PRECISIONS[symbol]
+  return `${parseFloat(amount).toFixed(precision)} ${symbol}`
+}
+
+/**
+ * Get amount removing all the extra zeroes of precision, e.g. 10000 XPR returns 1 (because XPR has a precision of 4 digits)
+ * @param amount The amount, e.g. 10001
+ * @param symbol The symbol, e.g. XPR
+ * @returns {Float} Real Amount human readable, e.g. 1.0001
+ */
+const getHumanFriendlyAmount = (blockchain_amount, symbol) => {
+  symbol = symbol.toUpperCase()
+  const precision = TOKEN_PRECISIONS[symbol]
+  return blockchain_amount / Math.pow(10, precision)
+}
+
 module.exports = {
   wait,
   getDeployableFilesFromDir,
-  sortDictionary
+  sortDictionary,
+  getQuantity,
+  getHumanFriendlyAmount
 }
