c825e19ef945eeaecd6e6a1eb0743b7964a9cf5f
cryptotester <49552918+cryptotester@users.noreply.github.com>
2021-04-22 22:56:34 +0200

    Allow to filter sales by currency symbol

--- nft/get-listings/index.js
+++ nft/get-listings/index.js
@@ -13,7 +13,7 @@ const getListings = async ({
     page = 1,
     limit = 100
 }) => {
-    const res = await fetch(`${NFT_API}/atomicmarket/v1/sales?seller=${seller}&collection_name=${collection_name}&template_id=${template_id}&ids=${sale_ids}&state=${state}&sort=${sort}&order=${order}&page=${page}&limit=${limit}`)
+    const res = await fetch(`${NFT_API}/atomicmarket/v1/sales?seller=${seller}&collection_name=${collection_name}&template_id=${template_id}&symbol=${symbol}&ids=${sale_ids}&state=${state}&sort=${sort}&order=${order}&page=${page}&limit=${limit}`)
     const { data } = await res.json()
     return data
 }
