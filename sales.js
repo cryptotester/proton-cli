50628c1cfbd00a60f2931e4071e78340d7363ab4
cryptotester <49552918+cryptotester@users.noreply.github.com>
2021-04-22 22:56:11 +0200

    Fix sales.js price displayed

--- sales.js
+++ sales.js
@@ -37,8 +37,9 @@ const main = async (account, collection_name, template_id) => {
     listings.forEach(l => {
         const key = `${l.assets[0].template.template_id} "${l.assets[0].template.immutable_data.name}"`
         const price = l.price.amount / Math.pow(10, l.price.token_precision)
+        const updateTime = new Date(parseInt(l.updated_at_time)).toISOString().slice(0, 19).replace('T', ' ')
         const sale_info = `asset_id: ${l.assets[0].asset_id}, template_mint: ${l.assets[0].template_mint}, sale_id: ${l.sale_id},`
-        + ` ${parseInt(price).toLocaleString()} ${l.price.token_symbol} by ${l.assets[0].owner}`
+        + ` ${parseFloat(price).toLocaleString()} ${l.price.token_symbol} by ${l.assets[0].owner}, ${updateTime}`
 
         if (key in groupedSales) {
             groupedSales[key].push(sale_info)
